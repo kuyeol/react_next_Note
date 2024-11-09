9d4e04fa26c751ac2cb577ac2e6d3708cc49e7e1
dev <125532184+kuyeol@users.noreply.github.com>
2024-11-09 23:13:02 +0900

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,15 +4,18 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://reactnextfront.vercel.app',
+    link: 'https://github.com/kuyeol/react_next_front',
   },
   chat: {
     link: 'https://github.com',
   },
-  docsRepositoryBase: 'https://github.com/kuyeol/react_next_front',
+  docsRepositoryBase: 'https://github.com/kuyeol/react_next_front/blob/main/pages',
   footer: {
     text: 'Next Docs Template',
   },
 }
+
+
+
 
 export default config
